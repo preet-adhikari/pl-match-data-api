48dacec017aa59ce110b076adf4277e17b6fd71b
Preet Adhikari <blazin.preet@gmail.com>
2023-02-21 21:34:51 +0545

    Added api routes and controller functions for authentication of user

---
+++ app/Http/Controllers/ApiAuthController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+use Illuminate\Support\Carbon as SupportCarbon;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+class ApiAuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        // Validate the data
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|unique:users,email|email',
+            'password' => 'required|confirmed|string|min:6'
+        ]);
+        // Create a new user
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password)
+        ]);
+        // Generate the token
+        $token = $user->createToken('Access Token');
+        // dd($token);
+        return response([
+            'access_token' => $token->accessToken,
+            'token_type' => 'Bearer',
+            'expires_at' => Carbon::parse($token->token->attributes['expires_at'])->toDateTimeString()
+        ], 200);
+    }
+
+    public function login(Request $request)
+    {
+        // Validate user
+        $request->validate([
+            'email' => 'required|string|email|max:255',
+            'password' => 'required|min:6|string|confirmed'
+        ]);
+        // Login user
+        if (Auth::attempt(['email' => $request->email, 'password' => $request->password]))
+        {
+            $token = Auth::user()->createToken("Access Token");
+            return response([
+                'access_token' => $token->accessToken,
+                'token_type' => 'Bearer',
+                'expires_at' => Carbon::parse($token->token->expires_at)->toDateTimeString()
+            ]);
+        } else {
+            return response([
+                'message' => 'The email or password is not correct.'
+            ], 422);
+        }
+    }
+
+    public function logout(Request $request)
+    {
+        $token = $request->user()->token();
+        //Revoke the access token
+        $token->revoke();
+        return response([
+            "message" => "You have successfully been logged out."
+        ], 200);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ApiAuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,6 +14,14 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+
+Route::post('login', [ApiAuthController::class, 'login'])->name('login');
+Route::post('register', [ApiAuthController::class, 'register'])->name('register');
+
+Route::group(['middleware' => 'auth:api'], function(){
+    Route::post('logout' , [ApiAuthController::class , 'logout'])->name('logout');
+});
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
